587e25ef9778877fd2cdfcb5b33213078cdfb2bf
effeto <effectdopler@gmail.com>
2021-10-11 11:25:43 +0300

    Places editor Sorting places by date/name
    End lesson 21

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -9,12 +9,18 @@ import UIKit
 import RealmSwift
 import SwiftUI
 
-class MainViewController: UITableViewController {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var reverseSortingButton: UIBarButtonItem!
     
     
     var places: Results<Place>!
+    var ascendingSorting = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +30,12 @@ class MainViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return places.isEmpty ? 0 : places.count
     }
 
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 
         let place = places[indexPath.row]
@@ -46,7 +52,7 @@ class MainViewController: UITableViewController {
     
     //MARK: Table View Delegate
     
-    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
     
     let place = places[indexPath.row]
     let deleteAction = UIContextualAction(style: .destructive, title: "Delete") {  (contextualAction, view, boolValue) in
@@ -54,6 +60,9 @@ class MainViewController: UITableViewController {
         StorageManager.deleteObject(place)
         tableView.deleteRows(at: [indexPath], with: .automatic)
     }
+         
+        
+        
     let swipeActions = UISwipeActionsConfiguration(actions: [deleteAction])
 
     return swipeActions
@@ -64,21 +73,50 @@ class MainViewController: UITableViewController {
         
     
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }#imageLiteral(resourceName: "Alaturka.jpeg")
-    */
+        if segue.identifier == "showDetail"{
+            guard let indexPath = tableView.indexPathForSelectedRow else {return}
+            let place = places[indexPath.row]
+            let newPlaceVc = segue.destination as! NewPlaceViewController
+            newPlaceVc.currentPlace = place
+             
+        }
+
+    }
+    
     
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         guard let newPlaceVC = segue.source as? NewPlaceViewController else {return}
         
-        newPlaceVC.saveNewPlace()
+        newPlaceVC.savePlace()
         tableView.reloadData()
     }
 
+    @IBAction func sortSelection(_ sender: UISegmentedControl) {
+        sorting()
+    }
+    
+    @IBAction func reverseSorting(_ sender: Any) {
+        
+        ascendingSorting.toggle()
+        
+        sorting()
+    }
+    
+    private func sorting() {
+        
+        if segmentedControl.selectedSegmentIndex ==  0 {
+            places = places.sorted(byKeyPath: "date", ascending: ascendingSorting)
+        }else {
+            places = places.sorted(byKeyPath: "name", ascending: ascendingSorting)
+        }
+        
+        tableView.reloadData()
+    }
+    
+    
 }
--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class NewPlaceViewController: UITableViewController {
     
     
+    
+    var currentPlace: Place?
     var imageIsChanged = false
     
     @IBOutlet weak var placeImage: UIImageView!
@@ -27,6 +29,7 @@ class NewPlaceViewController: UITableViewController {
         
         saveButton.isEnabled = false
         placeName.addTarget(self, action: #selector(textFieldChanged ), for: .editingChanged)
+        setupEditScreen()
     }
     
     //MARK: Table view delegate
@@ -52,7 +55,7 @@ class NewPlaceViewController: UITableViewController {
         }
     }
     
-    func saveNewPlace (){
+    func savePlace (){
         
         
         var image: UIImage?
@@ -70,9 +73,44 @@ class NewPlaceViewController: UITableViewController {
                              type: placeType.text,
                              imageData:imageData)
         
-        StorageManager.saveObject(newPlace)
+        if currentPlace != nil{
+            try! realm.write {
+                currentPlace?.name = newPlace.name
+                currentPlace?.location = newPlace.location
+                currentPlace?.type = newPlace.type
+                currentPlace?.imageData = newPlace.imageData
+            }
+        }else {
+            StorageManager.saveObject(newPlace)
+        }
+     
         
    }
+    
+    private func  setupEditScreen () {
+        if currentPlace != nil{
+            
+            setUpNavigationBar()
+            imageIsChanged = true
+            
+            guard let data = currentPlace?.imageData, let image = UIImage(data: data) else {return}
+            
+            placeImage.image = image
+            placeImage.contentMode = .scaleAspectFit
+            placeName.text = currentPlace?.name
+            placeType.text = currentPlace?.type
+            placeLocation.text = currentPlace?.location
+        }
+    }
+    
+    private func setUpNavigationBar () {
+        if let topItem = navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem =  UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
+        navigationItem.leftBarButtonItem = nil
+        title = currentPlace?.name
+        saveButton.isEnabled = true
+    }
     
     @IBAction func cancelAction(_ sender: Any) {
         dismiss(animated: true)
